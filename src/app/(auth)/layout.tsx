70522c46434faed8a7cf6a17eff2416a9348821c
raishudesu <bacaltosbaryshnikov@gmail.com>
2023-11-20 20:33:49 +0800

    updated auth page

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -9,11 +9,11 @@ const AuthLayout = async ({ children }: { children: ReactNode }) => {
   if (session) redirect("/dashboard");
   return (
     <section className="w-full flex flex-col justify-center items-center gap-6 mt-10 lg:mt-20">
-      <div className="w-full max-w-screen-xl grid md:grid-cols-2 justify-items-center items-center">
+      <div className="w-full max-w-screen-xl grid lg:grid-cols-2 justify-items-center items-center">
         <Image
           src={welcome}
           alt="welcome"
-          className="w-full max-w-xs lg:max-w-xl"
+          className="hidden lg:block w-full max-w-xs lg:max-w-xl"
         />
         {children}
       </div>
