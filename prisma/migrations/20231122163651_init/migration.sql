96ea785576da6f4562248205a92c6828f44d008d
raishudesu <bacaltosbaryshnikov@gmail.com>
2023-11-23 00:39:48 +0800

    updated models

---
+++ prisma/migrations/20231122163651_init/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "Comment" ADD COLUMN     "type" TEXT;
+
+-- AlterTable
+ALTER TABLE "Reply" ADD COLUMN     "type" TEXT;
+
+-- AlterTable
+ALTER TABLE "Thread" ADD COLUMN     "type" TEXT;
