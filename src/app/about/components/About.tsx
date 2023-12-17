0e897597378416f99ac3ef8f580057172aeb6b42
raishudesu <bacaltosbaryshnikov@gmail.com>
2023-12-17 15:58:34 +0800

    updated about page

--- src/app/about/components/About.tsx
+++ src/app/about/components/About.tsx
@@ -15,13 +15,13 @@ const About = () => {
         <CardTitle
           className={`scroll-m-20 text-3xl font-bold tracking-tight transition-colors first:mt-0`}
         >
-          About think<span className="text-yellow-500">sync</span>
+          About eStu<span className="text-yellow-500">dev</span>
         </CardTitle>
         <CardDescription className="leading-7 text-lg">
-          Thinksync is a community of tech students getting together to help one
-          another out. The tech industry relies on collaboration and networked
-          learning. We provide a place for students to enhance their learning
-          development for real-world scenarios.
+          Estudev is a community of student developers getting together to help
+          one another out. The tech industry relies on collaboration and
+          networked learning. We provide a place for students to enhance their
+          learning development for real-world scenarios.
           <br />
           <br />
           We believe in the power of collaboration on the tech development
